644d6ae8ab4f0abaeb81c4068f0636c3526717d1
pdelancy <pdelancy94@gmail.com>
2017-10-06 14:09:01 -0700

    visual

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var dummyData = ['Go to grocery store', 'Find license', 'Workout', 'Read a damn book']
 
-ReactDOM.render(<p>React lives!</p>,
-   document.getElementById('root'));
+
+class ToDoList extends React.Component {
+
+  render(){
+    return(
+      <ul>
+        {dummyData.map((task) => (<ToDo task={task}/>))}
+      </ul>
+    )
+  }
+}
+
+class ToDo extends React.Component {
+  render(){
+    return (
+      <li>
+        <button>X</button>
+        {this.props.task}
+      </li>
+    )
+  }
+}
+
+ReactDOM.render(< ToDoList />, document.getElementById('root'));
